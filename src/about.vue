73d60fc0835bc3301c1ea1b9521b16352f6ed5f2
reterw <wsq530@hotmail.com>
2022-08-05 18:28:09 -0400

    image

--- src/about.vue
+++ src/about.vue
@@ -4,63 +4,77 @@
         <div>
             <div>
                 <br />
-                <label class="text-6xl">Some details about this app</label>
+                <label class="text-6xl">Survey Introduction</label>
                 <br />
-                <label class="text-4xl">Azure function</label>
+                <label class="text-4xl">Login</label>
                 <br />
-                <label class="text-2xl">My Server-Side functionality has been deployed on Microsoft Azure server</label>
+                <label class="text-2xl">Press the login button on the up right corner</label>
                 <br />
-                <img src="./image/602/602azureFunction.jpg" />
-                <label class="text-4xl">Persistence</label>
-                <br />
-                <label class="text-2xl">Im using CusmosDB provide by Azure to save my data, which also share the Mongo API</label>
-                <br />
-                <img src="./image/602/cusmosdb.jpg" />
-                <label class="text-4xl">Rest API endpoints</label>
-                <br />
-                <a
-                    class="text-3xl text-yellow-200"
-                    href="https://602vote.azurewebsites.net/api/vote"
-                >https://602vote.azurewebsites.net/api/vote</a>
-                <img src="./image/602/RestEndpoints.jpg" />
-
-                <label class="text-4xl">Log in with github Oauth</label>
-                <br />
-                <br />
-                <img src="./image/602/githuboauth.jpg" />
+                <img src="./image/673/1-673login.jpg" />
                 
-                <label class="text-4xl">Home Page render all user's vote together, and the vote box does not have delete button</label>
                 <br />
+                <label class="text-2xl">login with github Oauth</label>
                 <br />
-                <img src="./image/602/homepage.jpg" />
+                <img src="./image/673/2-githuboauth.jpg" />
+                <br />
+                <label class="text-2xl">clcick survey on top right, and choose create survey, you can only create survey after login</label>
+                <br />
+                <img src="./image/673/3-createsurvey.jpg" />
+                <br />
+                <label class="text-2xl">click type, add survey name first</label>
+                <br />
+                <img src="./image/673/4-surveyname.jpg" />
+                <br />
+                <label class="text-2xl">type survey name, then click add to add the title</label>
+                <img src="./image/673/4-surveyname2.jpg" />
+                <br />
+                <label class="text-2xl">add for add, X for delete</label>
+                <img src="./image/673/4-surveyname3.jpg" />
+                <br />
+                <label class="text-2xl">from type, choose single</label>
+                <img src="./image/673/5-single1.jpg" />
+                <br />
+                <label class="text-2xl">tile, input the question, and give the choice,"+" for add more choice, add for add in to question list</label>
+                <img src="./image/673/5-single.jpg" />
+                <br />
+                <label class="text-2xl">after click add, question will add to the list, click X to delete</label>
+                <img src="./image/673/5-single2.jpg" />
+                <br />
+                <label class="text-2xl">use the similar way to add the multiple choice and short answer question</label>
+                <img src="./image/673/6-muitiple.jpg" />
+                <img src="./image/673/7-input.jpg" />
+                <br />
+                <label class="text-2xl">press Create publish the survey</label>
+                <img src="./image/673/8-thesurvey.jpg" />
+                <br />
+                <label class="text-2xl">Click home to see survey, and click title to get in survey</label>
+                <img src="./image/673/9-dosurvey.jpg" />
+                <br />
+                <label class="text-2xl">finish the survey, then click submic</label>
+                <img src="./image/673/9-dosurvey2.jpg" />
 
-                <label class="text-4xl">my page is for admin, after you login , you can render the vote create by you, and also delete them by click the button.</label>
                 <br />
+                <label class="text-2xl">back to home page click "view Statistic" or "View All Result" to check the how other people do the survey</label>
+                <img src="./image/673/10-review.jpg" />
                 <br />
-                <img src="./image/602/mypage.jpg" />
+                <label class="text-2xl">view Statistic</label>
+                <img src="./image/673/10-review1.jpg" />
+                <br />
+                <label class="text-2xl">View All Result</label>
+                <img src="./image/673/10-review2.jpg" />
+                <br />
+                <label class="text-2xl">after you login, click "my page" on the left, you can delete the survey create by your own by press "X" </label>
+                <img src="./image/673/11-delete.jpg" />
+
+
+
+
+
+
+
+
+
                
-                <label class="text-4xl">for create vote, enter the title of the vote problem, and enter choice of it, then click submit.</label>
-                <br />
-                <br />
-                <img src="./image/602/createvote.jpg" />
-                <img src="./image/602/createvote2.jpg" />
-
-                <label class="text-4xl">click thumb to vote your choice.</label>
-                <br />
-                <br />
-                <img src="./image/602/vote1.jpg" />
-                <img src="./image/602/vote2.jpg" />
-
-                <label class="text-4xl">vote count will change in data base, and each prople's vote are recorded.</label>
-                <br />
-                <br />
-                <img src="./image/602/votecount.jpg" />
-                <img src="./image/602/votehistory.jpg" />
-
-                <label class="text-4xl">using delete, get, post and put methods.</label>
-                <br />
-                <br />
-                <img src="./image/602/restapi.jpg" />
 
             </div>
         </div>
